f4ef52f0d2e1e7688a4ea7333048fc86094990b7
DKelley <dianna.kelley@gmail.com>
2018-01-16 20:32:23 -0800

    Add project files.

---
+++ FirstChallenge/Controllers/ComicBookController.cs
@@ -0,0 +1,20 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ActionResult Index(int ComicBookId) => View();
+
+
+       
+
+    }
+   
+}
---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstChallenge.Models;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var ComicBooks = ComicBookManager.GetComicBooks();
+            return View(ComicBooks);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var ComicBooks = ComicBookManager.GetComicBooks();
+            var comicBook = ComicBooks.FirstOrDefault(p=> p.ComicBookId == id);
+
+            return View(comicBook);
+        }
+    }
+}
